37d72183615fae740e8053b107488a7b09f67e4b
miladgulnoori <miladgulnoori@gmail.com>
2025-11-07 22:19:26 +0430

    Creating employee route page

---
+++ packages/server/src/routers/employee.route.js
@@ -0,0 +1,28 @@
+import { Router } from "express";
+
+const employeeRouter = Router();
+
+// Create a new employee
+employeeRouter.post("/", async (req, res) => {
+  // body: {name, email, department}
+  res.send({ success: true, message: "Employee created!" });
+});
+
+//Get all emoployees
+employeeRouter.get("/", async (req, res) => {
+  res.send({ success: true, data: [] });
+});
+
+//Get single emaployee by Id
+employeeRouter.get("/:Id", async (req, res) => {
+  const { Id } = req.params;
+  res.send({ success: true, data: { Id } });
+});
+
+//Update employee
+employeeRouter.put("/:Id", async (req, res) => {
+  const { Id } = req.params;
+  res.send({ success: true, message: `Employee ${Id} updated!` });
+});
+
+export default employeeRouter;
