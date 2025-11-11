db90f8667c4c2a5266585b5952a6d2c99778ed11
miladgulnoori <miladgulnoori@gmail.com>
2025-11-11 23:08:51 +0430

    editing Errorpage

--- app/web/src/pages/ErrorPage.jsx
+++ app/web/src/pages/ErrorPage.jsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button";
+import { Button } from "/components/ui/button";
 import { isRouteErrorResponse, Link, useRouteError } from "react-router-dom";
 
 const ErrorPage = () => {
