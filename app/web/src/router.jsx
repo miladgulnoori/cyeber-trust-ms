16a589ac1c96c2c9896681ff31b47147dd84e908
miladgulnoori <miladgulnoori@gmail.com>
2025-11-16 22:34:11 +0430

    Creating AppLayout

--- app/web/src/router.jsx
+++ app/web/src/router.jsx
@@ -1,16 +1,17 @@
 // src/routes/index.jsx
 import { lazy } from "react";
 import { createBrowserRouter, Navigate } from "react-router-dom";
-// import AppLayout from "/components/Layout"; // your layout with sidebar + navbar
+import AppLayout from "@/components/AppLayout"; // your layout with sidebar + navbar
+import ErrorPage from "@/pages/ErrorPage";
 
 // Lazy-load pages for performance
-// const DashboardPage = lazy(() => import("./pages/DashboardPage"));
-const Employee = lazy(() => import("./pages/Employee"));
-const Department = lazy(() => import("./pages/Department"));
-const Task = lazy(() => import("./pages/Task"));
-const Project = lazy(() => import("./pages/Project"));
-const Quotations = lazy(() => import("./pages/Quotations"));
-const CostTracking = lazy(() => import("./pages/CostTracking"));
+// const DashboardPage = lazy(() => import("@/pages/DashboardPage"));
+const EmployeeManagementPage = lazy(() => import("@/pages/Employee"));
+const DepartmentManagementPage = lazy(() => import("@/pages/Department"));
+const TasksManagementPage = lazy(() => import("@/pages/Task"));
+const ProjectsManagementPage = lazy(() => import("@/pages/Project"));
+const QuotationsPage = lazy(() => import("@/pages/Quotations"));
+const CostTrackingPage = lazy(() => import("@/pages/CostTracking"));
 
 // Optional: authentication layout if needed later
 // const AuthLayout = lazy(() => import('@/pages/auth/AuthLayout'));
@@ -36,27 +37,27 @@ const router = createBrowserRouter([
           },
           {
             path: "employees",
-            element: <Employee />,
+            element: <EmployeeManagementPage />,
           },
           {
             path: "departments",
-            element: <Department />,
+            element: <DepartmentManagementPage />,
           },
           {
             path: "tasks",
-            element: <Task />,
+            element: <TasksManagementPage />,
           },
           {
             path: "projects",
-            element: <Project />,
+            element: <ProjectsManagementPage />,
           },
           {
             path: "quotations",
-            element: <Quotations />,
+            element: <QuotationsPage />,
           },
           {
             path: "cost-tracking",
-            element: <CostTracking />,
+            element: <CostTrackingPage />,
           },
         ],
       },
